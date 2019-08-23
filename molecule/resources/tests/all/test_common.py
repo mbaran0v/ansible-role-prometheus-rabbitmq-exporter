d5da3ad78610e5c6b67bd42236e4aeccbf117671
Maxim Baranov <maksim.baranov@gmail.com>
2019-08-23 13:54:32 +0400

    improve tests, update versions (#1)

--- molecule/resources/tests/all/test_common.py
+++ molecule/resources/tests/all/test_common.py
@@ -1,6 +1,13 @@
 
 debian_os = ['debian', 'ubuntu']
 rhel_os = ['redhat', 'centos']
+version = '0.29.0'
+root_dir = '/opt/rabbitmq_exporter'
+config_file = root_dir + '/releases/' + version + '/config'
+service_name = 'rabbitmq_exporter'
+user_name = 'rabbitmq-exp'
+group_name = user_name
+socket = 'tcp://0.0.0.0:9419'
 
 
 def test_distribution(host):
@@ -8,54 +15,44 @@ def test_distribution(host):
 
 
 def test_install_dir(host):
-    f = host.file('/opt/rabbitmq_exporter')
+    f = host.file(root_dir)
 
     assert f.exists
     assert f.is_directory
 
 
-def test_config_file(host):
-    f = host.file('/opt/rabbitmq_exporter/shared/config')
-
-    assert f.exists
-    assert f.is_file
-
-
 def test_release_dir(host):
-    f = host.file('/opt/rabbitmq_exporter/releases/0.29.0')
+    f = host.file(root_dir + '/releases/' + version)
 
     assert f.exists
     assert f.is_directory
 
 
 def test_release_symlink_dir(host):
-    f = host.file('/opt/rabbitmq_exporter/current')
+    f = host.file(root_dir + '/current')
 
     assert f.exists
     assert f.is_symlink
-    assert f.linked_to == '/opt/rabbitmq_exporter/releases/0.29.0'
+    assert f.linked_to == root_dir + '/releases/' + version
 
 
 def test_service(host):
-    s = host.service('rabbitmq_exporter')
+    s = host.service(service_name)
 
     assert s.is_enabled
     assert s.is_running
 
 
-def test_socket(host):
-    s = host.socket('tcp://0.0.0.0:9419')
-
-    assert s.is_listening
-
-
 def test_user(host):
-    u = host.user('rabbitmq-exp')
+    u = host.user(user_name)
 
+    assert u.exists
+    assert u.group == group_name
     assert u.shell == '/usr/sbin/nologin'
 
 
-def test_group(host):
-    g = host.user('rabbitmq-exp')
+def test_config(host):
+    f = host.file(config_file)
 
-    assert g.exists
+    assert f.exists
+    assert oct(f.mode) == '0o600'
